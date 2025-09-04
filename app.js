01754365de18b57afe7468ffdc2b9fc08618e9cf
Sakshi <Sakshiii.t17@gmail.com>
2025-09-04 22:35:44 +0530

    basic routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");
+const Listing = require("./models/listing.js");
+const path = require("path");
 
 // DATABASE CONNECTION
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -15,14 +16,33 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
 
+// app.get("/testListing", async (req,res) => { 
+// let sampleListing = new Listing({
+//     title: "my home",
+//     description: "by the beach",
+//     price: 1200,
+//     location: "goa",
+//     country: "india"
+// });
+//    await sampleListing.save();
+//    console.log("sample was saved");
+//    res.send("successful listing");
+// });
 
-app.get("/test")
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index", {allListings});
+});
 
-
-
-
-
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
 
 
 app.get("/", (req,res) => {
--- models/listing.js
+++ models/listing.js
@@ -8,13 +8,18 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type:String,
-        set: (v) => v === "" ? "https://unsplash.com/photos/a-lake-surrounded-by-mountains-and-trees-under-a-blue-sky-jV8916l2k0I" : v,
-    },
+        filename: {
+        type: String,
+        default: "listingimage"
+         },
+        url: {
+        type: String,
+        default: "https://unsplash.com/photos/a-lake-surrounded-by-mountains-and-trees-under-a-blue-sky-jV8916l2k0I"}
+        },
     price: Number,
     location: String,
     country: String
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-modules.export = Listing;
+module.exports = Listing;
